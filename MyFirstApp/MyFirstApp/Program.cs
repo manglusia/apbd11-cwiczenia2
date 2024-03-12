27feabbfdcb2aebec4ecc1326cc388ca06f08e06
manglusia <stasiek.rupala@gmail.com>
2024-03-12 15:14:49 +0100

    Add GetAverage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -2,3 +2,14 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
+
+static double GetAverage(int[]tab)
+{
+    double sum = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        sum += tab[i];
+    }
+
+    return sum / tab.Length;
+}
